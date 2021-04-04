7bff5fffe0cabf42d10f988fe4f4ba87cfd67fa3
Irina <irina.kanus@gmail.com>
2021-04-04 07:48:58 +0300

    Implemented creation of new record in database from REST api

--- src/main/java/irina/salon/helper/models/Clients.java
+++ src/main/java/irina/salon/helper/models/Clients.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Data
@@ -15,7 +12,6 @@ import javax.persistence.Id;
 @NoArgsConstructor
 public class Clients {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     private String name;
     private String phone;
